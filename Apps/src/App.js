9f8f47edff037b4c6e7d234a9d0398fda0b2fef4
Jarred <jarred_mcmahan@yahoo.com>
2021-04-26 19:55:25 -0500

    Bootstrap setup

---
+++ Apps/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style/App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+      <h2>Project portfolio</h2>
+  );
+}
+
+export default App;
